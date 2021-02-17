15e1d9376851dc4f7704ebfa9fce2000176b1d6e
p17204296 <45291715+p17204296@users.noreply.github.com>
2021-02-17 12:27:37 +0000

    P17204296 - Development Project

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html xmlns="http://w3.org/1999/xhtml">
+
+<head>
+<title>Flexi-Hires - CTEC3451-Development Project</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<meta name="keywords" content="Freelancing; Flexi-Hires" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+<div id="header">
+
+<div id="logo"><a href="index.html"><img src="assets/images/logo_land.png" width="224" height="97"></a></div>
+
+<div id="nav">
+		<ul>	
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about.html">Register/Login</a></li>
+		<ul>	
+</div>
+
+
+</div>
+
+
+<div id="wrapper">
+
+<div id="hm_head"><h3>Flexi Hires is your go to freelancing platform </h3> </div>
+<table id="table1" cellspacing="40">
+	<tr>
+		<td><p>Hire our talented freelancers today!</p><button>Get Started!</button></td>
+		<td><h3>IMAGE</h3></td>
+	</tr>
+</table>
+
+<table id="table1" cellspacing="40">
+	<tr>
+		<td><h3 class="Home">IMAGE</h3> <p class="p1"></p> </td>
+		<td><h3 class="About">IMAGE</h3> </td>
+		<td><h3 class="Contact">IMAGE</h3><p class="p2"></p> </td>
+	</tr>
+</table>
+
+	<div id="footer">&copy; 2021 All rights reserved.</div>
+
+
+</body>
+
+
+<html>
