8f701a0d0ea2b62ed7383566d64a6903c05626bf
p17204296 <45291715+p17204296@users.noreply.github.com>
2021-03-25 17:44:15 +0000

    removed index variable

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php include('partials/indexHeader.php');
-$index=false;
-?>
+<?php include('partials/indexHeader.php');?>
 
 	<!-- Category Search Section Starts-->
 	<section class="text-align-center main-content-index">
