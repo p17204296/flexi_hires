0abf4306a99c82f442b38c86be227ef1cf98c97f
p17204296 <45291715+p17204296@users.noreply.github.com>
2021-03-25 17:25:22 +0000

    CSS changes to index - Mobile Responsiveness

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
-<?php include('partials/header.php'); ?>
+<?php include('partials/indexHeader.php');
+$index=false;
+?>
 
-<!-- Category Search Section Starts-->
-	<section class="cat-search text-align-center">
-		<div class="container">
-			<div class="bg-wrapper">
-				<div class="bg-container">
-					<div class="bg2-div">
-						<!-- <img src="images/bg-2.png" alt="Flexi-Hires Logo" class="bg2" > -->
-					</div>
-				</div>
-			</div>
-			<form class="" action="index.html" method="post">
-				<input type="search" name="search" placeholder="Try 'web development' ">
-				<input type="submit" name="submit" value="Search" class="btn btn-primary-col">
-			</form>
-		</div>
+	<!-- Category Search Section Starts-->
+	<section class="text-align-center main-content-index">
+	<h1>FLEXI-HIRES - Find the perfect freelance services for your business</h1>
+	<form class="cat-search" action="index.html" method="post">
+		<input type="search" name="search" placeholder="Try 'web development' ">
+		<input type="submit" name="submit" value="Search" class="index-search-btn btn-primary-col">
+	</form>
 	</section>
-<!-- Category Search Section Ends -->
+	<!-- Category Search Section Ends -->
+</section>
 
 <!-- Category Section Starts-->
 	<section class="categories">
@@ -24,15 +19,22 @@
 			<h2 class="text-align-center">Categories</h2>
 
 			<div class="cat-boxes float-container">
-				<img src="assets/images/seo@2x.webp" alt="SEO Category" class="responsive-img curve-img">
-				<h3 class="cat-deco float-text purple-text ">SEO Category</h3>
+				<a href="#">
+					<img src="assets/images/seo@2x.webp" alt="SEO Category" class="responsive-img curve-img">
+					<h3 class="cat-deco float-cat-text purple-text text-align-center">SEO & SEM</h3>
+				</a>
 			</div>
 			<div class="cat-boxes float-container">
-				<img src="assets/images/videography@1x.jpg" alt="videography Category" class="responsive-img">
-
+				<a href="#">
+					<img src="assets/images/videography@1x.jpg" alt="videography Category" class="responsive-img curve-img">
+					<h3 class="cat-deco float-cat-text purple-text text-align-center">Videography</h3>
+				</a>
 			</div>
 			<div class="cat-boxes float-container">
-				<img src="assets/images/content-writing@2x.webp" alt="Content Writing Category" class="responsive-img">
+				<a href="#">
+					<img src="assets/images/content-writing@2x.webp" alt="Content Writing Category" class="responsive-img curve-img">
+					<h3 class="cat-deco float-cat-text purple-text text-align-center ">Content Writing</h3>
+				</a>
 			</div>
 			<div class="clearfix"></div>
 		</div>
@@ -43,9 +45,28 @@
 	<section class="explore-section">
 		<div class="container">
 			<h2 class="text-align-center">Explore Categories</h2>
+
 			<div class="explore-boxes">
-				Box 1
+				<img src="assets/images/content-writing@2x.webp" alt="Content Writing Category" class="explore-img responsive-img curve-img">
+				<div class="explore-info">
+					<h3>Content Writing</h4>
+					<p class="cat-details">Content Writing Freelancers available</p>
+					<br>
+					<a href="#" class="btn btn-primary-col">View Category</a>
+				</div>
 			</div>
+
+			<div class="explore-boxes">
+				<img src="assets/images/content-writing@2x.webp" alt="Content Writing Category" class="explore-img responsive-img curve-img">
+				<div class="explore-info">
+					<h3>Content Writing</h4>
+					<p class="cat-details">Content Writing Freelancers available</p>
+					<br>
+					<a href="#" class="btn btn-primary-col">View Category</a>
+				</div>
+			</div>
+
+
 			<div class="explore-boxes">
 				Box 2
 			</div>
@@ -55,10 +76,11 @@
 			<div class="explore-boxes">
 				Box 4
 			</div>
-
+			<div class="clearfix"></div>
 		</div>
 		<div class="clearfix"></div>
 	</section>
 <!-- Explore Section Ends -->
+
 
 <?php include('partials/footer.php'); ?>
