7e0ebd55624b9e9401d1630b7e67094fec38c3e0
p17204296 <45291715+p17204296@users.noreply.github.com>
2021-05-20 03:30:52 +0100

    Sanitised POST data for post project

--- app/controllers/postProject.php
+++ app/controllers/postProject.php
@@ -17,9 +17,12 @@ class postProject extends Controller
         //Load postProjectModel
         if (isset($_POST['postProject'])) {
             $advertiseProject = $this->loadModel("projectModel");
-
-            //Post Project Function
-            $advertiseProject->postProjectSQL($_POST);
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                //Post Project Function
+                $advertiseProject->postProjectSQL($_POST);
+            }
         }
 
         $this->view("postProjectView", $data);
